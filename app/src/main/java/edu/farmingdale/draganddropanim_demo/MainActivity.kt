ce1b733e95f7ca87ae0595d14ff4e7cbe32bbd2c
Himal <104921088+9bitbin@users.noreply.github.com>
2024-12-07 10:34:17 -0500

    adding comments to the MainActivity.kt

--- app/src/main/java/edu/farmingdale/draganddropanim_demo/MainActivity.kt
+++ app/src/main/java/edu/farmingdale/draganddropanim_demo/MainActivity.kt
@@ -1,21 +1,30 @@
 package edu.farmingdale.draganddropanim_demo
-
+/**
+ * Name: Himal Shrestha
+ * Course: BCS 371 - Mobile Appliation Development
+ * Prof: Alrajab
+ * Week 12
+ */
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import edu.farmingdale.draganddropanim_demo.ui.theme.DragAndDropAnim_DemoTheme
 
+// MainActivity serves as the entry point of the application.
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Enables edge-to-edge display for the activity.
         enableEdgeToEdge()
+
+        // Sets the content view using Jetpack Compose.
         setContent {
             DragAndDropAnim_DemoTheme {
 
-                    DragAndDropBoxes()
-
-
+                // Displays the DragAndDropBoxes composable, which is the core UI for the demo.
+                DragAndDropBoxes()
             }
         }
     }
@@ -31,7 +40,7 @@ class MainActivity : ComponentActivity() {
 // ToDo x: Make this works in landscape mode only
 // ToDo x: Rotate the rect around itself
 // ToDo x: Move - translate the rect horizontally and vertically
-// ToDo 8: Add a button to reset the rect to the center of the screen
+// ToDo x: Add a button to reset the rect to the center of the screen
 // ToDo 9: Enable certain animation based on the drop event (like up or down)
 // ToDo 10: Make sure to commit for each one of the above and submit this individually
 
